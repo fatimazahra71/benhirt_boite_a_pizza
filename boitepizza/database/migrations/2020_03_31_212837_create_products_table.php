1fde25062e65064da50ad71150621fc6be8b81dd
Benhirt Fatima Zahra <fatimazahra9630@gmail.com>
2020-04-02 01:37:36 +0100

    commit

---
+++ boitepizza/database/migrations/2020_03_31_212837_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom')->unique();
+            $table->float('prix');
+            $table->integer('remise');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->unsignedInteger('categoryID');
+            $table->foreign('categoryID')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
